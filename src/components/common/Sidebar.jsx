e9812ce7f6ed3a19e4760c9318b15a9179e9c6e2
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-07-26 21:04:08 +0530

    Sidebar

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -3,35 +3,42 @@ import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className="bg-gray-800 text-white w-64 h-full p-5">
-      <h2 className="text-xl font-bold mb-4">StreetSathi</h2>
+    <div className="bg-gray-800 text-white w-64 h-full p-5 border-r-2 border-[#ffffff10]">
+      <h2 className="text-3xl font-bold mb-4 border-b-2 border-[#ffffff10] text-center pb-4">
+        StreetSathi
+      </h2>
       <ul>
-        <li className="mb-2">
+        <li className="mb-2 text-lg">
           <Link to="/dashboard" className="hover:text-gray-400">
             Dashboard
           </Link>
         </li>
-        <li className="mb-2">
+        <li className="mb-2 text-lg">
           <Link to="/inventory" className="hover:text-gray-400">
             Inventory
           </Link>
         </li>
-        <li className="mb-2">
+        <li className="mb-2 text-lg">
           <Link to="/orders" className="hover:text-gray-400">
             Orders
           </Link>
         </li>
-        <li className="mb-2">
+        <li className="mb-2 text-lg">
+          <Link to="/revenue" className="hover:text-gray-400">
+            Revenue
+          </Link>
+        </li>
+        <li className="mb-2 text-lg">
           <Link to="/profile" className="hover:text-gray-400">
             Profile
           </Link>
         </li>
-        <li className="mb-2">
+        <li className="mb-2 text-lg">
           <Link to="/cost-split" className="hover:text-gray-400">
             Cost Split
           </Link>
         </li>
-        <li className="mb-2">
+        <li className="mb-2 text-lg">
           <Link to="/delivery-status" className="hover:text-gray-400">
             Delivery Status
           </Link>
