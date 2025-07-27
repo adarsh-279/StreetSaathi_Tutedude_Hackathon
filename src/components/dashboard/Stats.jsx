b9c7051e6d4fcbd7c80da15693769d72dd1c2a33
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-07-27 13:18:58 +0530

    Updated

--- src/components/dashboard/Stats.jsx
+++ src/components/dashboard/Stats.jsx
@@ -3,19 +3,19 @@ import React from "react";
 const Stats = () => {
   return (
     <div className="stats-container">
-      <h2 className="text-xl font-bold">Key Statistics</h2>
+      <h2 className="text-xl font-bold mb-2">Key Statistics</h2>
       <div className="stats-grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 text-black">
         <div className="stat-card bg-white shadow-lg rounded-lg p-4">
           <h3 className="text-lg">Total Inventory Items</h3>
-          <p className="text-2xl font-semibold">150</p>
+          <p className="text-2xl font-semibold">3</p>
         </div>
         <div className="stat-card bg-white shadow-lg rounded-lg p-4">
           <h3 className="text-lg">Total Orders Today</h3>
-          <p className="text-2xl font-semibold">25</p>
+          <p className="text-2xl font-semibold">5</p>
         </div>
         <div className="stat-card bg-white shadow-lg rounded-lg p-4">
           <h3 className="text-lg">Pending Deliveries</h3>
-          <p className="text-2xl font-semibold">5</p>
+          <p className="text-2xl font-semibold">3</p>
         </div>
       </div>
     </div>
