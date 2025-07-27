0b32b1fe5e275d36c2c157ae09516a848ffd7af4
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-07-27 12:17:21 +0530

    Routing

--- src/utils/Routing.jsx
+++ src/utils/Routing.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Dashboard from '../pages/Dashboard';
-import InventoryCard from '../components/dashboard/InventoryCard';
+import Inventory from '../pages/Inventory';
 import OrderList from '../components/dashboard/OrderList';
 import Profile from '../pages/Profile'
+import DashboardPage from '../components/dashboard/DashboardPage'
+import UsersList from '../components/dashboard/UsersList';
+import RevenuePage from '../components/dashboard/RevenuePage';
+import DelhiveryPage from '../components/dashboard/DelhiveryPage';
 
 const Routing = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/inventory" element={<InventoryCard />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/inventory" element={<Inventory />} />
         <Route path="/orders" element={<OrderList />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/users" element={<UsersList />} />
+        <Route path="/revenue" element={<RevenuePage />} />
+        <Route path="/delhivery" element={<DelhiveryPage />} />
       </Routes>
     </>
   );
